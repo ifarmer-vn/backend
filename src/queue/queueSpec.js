685f17deb849ef0c5b54a8a0aab285ccf06d5707
Hai Bui <hoanghaibui2003@gmail.com>
2019-08-09 01:04:06 +0800

    Connect to MongoDB Atlas

--- src/queue/queueSpec.js
+++ src/queue/queueSpec.js
@@ -2,7 +2,7 @@ const queue = require("./queue");
 
 describe("queue", () => {
     describe("createQueue", () => {
-        fit("should create queue correctly", async () => {
+        it("should create queue correctly", async () => {
             const input = "test_queue";
             const output = queue.createQueue(input);
             expect({}).toEqual(output);
